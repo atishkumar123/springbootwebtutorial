b06346c864870091e0d84600644fd6630a361342
atish.k <atishkumar12548@gmail.com>
2025-02-17 21:32:07 +0530

    ss

--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,4 +1,8 @@
 package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+
 }
