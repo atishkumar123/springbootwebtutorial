67e777179f7aed01af505a1debd9835a305bd876
atish.k <atishkumar12548@gmail.com>
2025-02-18 13:51:02 +0530

    18feb

--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,9 +1,15 @@
 package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
-    //abc
+
+    @GetMapping(path="/getSecretMessage")
+    public String getMySuperSecretMessage(){
+        return "Secret Message:abc";
+    }
+
 
 }
